f195da021f192f9824b5d1afa25549246f00f0b0
Имя Фамилия <alexandrmekhaev@yandex.ru>
2025-10-08 00:12:17 +0300

    features/login

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const { login } = require('./controllers/users');
+
 const app = express();
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/tasksdb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signin', login);
+
 
 app.use((req, res, next) => { 
   req.user = { 
--- controllers/users.js
+++ controllers/users.js
@@ -78,3 +78,30 @@ module.exports.deleteUser = (req, res) => {
       res.status(500).send({ message: err.message }); 
     }); 
 };
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        // хеши не совпали — отклоняем промис
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      // аутентификация успешна
+      res.send({ message: 'Всё верно!' });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+}; 
